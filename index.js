809064afb24615bdcfe192c23ab30fd469a8febb
Rikerz08 <rikikarl8@gmail.com>
2022-01-07 11:34:01 +0800

    add to list function added

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-(function() {
-  
-    var slideContainer = $('.slide-container');
-    
-    slideContainer.slick();
-    
-    $('.card__image img').hide();
-    $('.slick-active').find('.card img').fadeIn(200);
-    
-    // On before slide change
-    slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-      $('.slick-active').find('.card img').fadeOut(1000);
-    });
-    
-    // On after slide change
-    slideContainer.on('afterChange', function(event, slick, currentSlide) {
-      $('.slick-active').find('.card img').fadeIn(200);
-    });
-    
-  })();
-  
+(function () {
+  var slideContainer = $(".slide-container");
+
+  slideContainer.slick();
+
+  $(".card__image img").hide();
+  $(".slick-active").find(".card img").fadeIn(200);
+
+  // // On before slide change
+  slideContainer.on(
+    "beforeChange",
+    function (event, slick, currentSlide, nextSlide) {
+      $(".slick-active").find(".card img").fadeOut(1000);
+    }
+  );
+
+  // On after slide change
+  slideContainer.on("afterChange", function (event, slick, currentSlide) {
+    $(".slick-active").find(".card img").fadeIn(200);
+  });
+})();
+
+// (function () {
+//   $('.slide-container').slick({
+//     rtl: true
+//   });
+
 // need a function for creating li, div, animal-img, name-span, heart image
 //  we'll have 10 variables for the 10 heart buttons for animals then we
 // make the button1.onclick, button2.onclick and input in the brackets the createPetList func
@@ -34,72 +40,72 @@ const happyButton = document.getElementById("happy-btn");
 const scarButton = document.getElementById("scar-btn");
 const simbaButton = document.getElementById("simba-btn");
 const ul = document.getElementById("pet-list");
+const removeButton = document.getElementsByClassName("removeBtn");
+
+removeButton.onclick;
 
 function createPetList(name, image) {
+  const displayName = document.getElementById(name);
   const li = document.createElement("li");
   const div = document.createElement("div");
   const animalImg = document.createElement("img");
-  animalImg.setAttribute("src", image.value);
+  animalImg.setAttribute("src", document.getElementById(image).src);
+  animalImg.className = "animal";
   const span = document.createElement("span");
-  span.textContent = name.value;
+  span.innerHTML = displayName.innerHTML;
   const heartImg = document.createElement("img");
+  heartImg.className = "heart";
   heartImg.setAttribute("src", "images/heart.png");
+  const removeButton = document.createElement("button");
+  removeButton.innerHTML = "Remove";
+  removeButton.className = "removeBtn";
 
   li.appendChild(div);
   div.appendChild(animalImg);
   div.appendChild(span);
   div.appendChild(heartImg);
+  div.appendChild(removeButton);
 
   return li;
 }
 
 albinaButton.onclick = function () {
-  createPetList("#albina-name", "#albina-img");
-  const li = createPetList();
+  const li = createPetList("albina-name", "albina-img");
   ul.appendChild(li);
 };
 mugButton.onclick = function () {
-  createPetList("#mug-name", "#mug-img");
-  const li = createPetList();
+  const li = createPetList("mug-name", "mug-img");
   ul.appendChild(li);
 };
 casperButton.onclick = function () {
-  createPetList("#casper-name", "#casper-img");
-  const li = createPetList();
+  const li = createPetList("casper-name", "casper-img");
   ul.appendChild(li);
 };
 cottonButton.onclick = function () {
-  createPetList("#cotton-name", "#cotton-img");
-  const li = createPetList();
+  const li = createPetList("cotton-name", "cotton-img");
   ul.appendChild(li);
 };
 edgarButton.onclick = function () {
-  createPetList("#edgar-name", "#edgar-img");
-  const li = createPetList();
+  const li = createPetList("edgar-name", "edgar-img");
   ul.appendChild(li);
 };
 georgeButton.onclick = function () {
-  createPetList("#george-name", "#george-img");
-  const li = createPetList();
+  const li = createPetList("george-name", "george-img");
   ul.appendChild(li);
 };
 grumpyButton.onclick = function () {
-  createPetList("#grumpy-name", "#grumpy-img");
-  const li = createPetList();
+  const li = createPetList("grumpy-name", "grumpy-img");
   ul.appendChild(li);
 };
 happyButton.onclick = function () {
-  createPetList("#happy-name", "#happy-img");
-  const li = createPetList();
+  const li = createPetList("happy-name", "happy-img");
   ul.appendChild(li);
 };
 scarButton.onclick = function () {
-  createPetList("#scar-name", "#scar-img");
-  const li = createPetList();
+  const li = createPetList("scar-name", "scar-img");
   ul.appendChild(li);
 };
 simbaButton.onclick = function () {
-  createPetList("#simba-name", "#simba-img");
-  const li = createPetList();
+  const li = createPetList("simba-name", "simba-img");
   ul.appendChild(li);
 };
